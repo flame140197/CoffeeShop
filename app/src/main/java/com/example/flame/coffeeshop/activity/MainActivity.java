07ca5435fddd938331b55f767327f5e8d9df2af6
Nhân Đoàn <flame140197@users.noreply.github.com>
2018-12-12 22:06:43 +0700

    LoadDuLieuThanhMenu

--- app/src/main/java/com/example/flame/coffeeshop/activity/MainActivity.java
+++ app/src/main/java/com/example/flame/coffeeshop/activity/MainActivity.java
@@ -14,8 +14,23 @@ import android.widget.ListView;
 import android.widget.ViewFlipper;
 import android.support.v7.widget.Toolbar;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.flame.coffeeshop.R;
+import com.example.flame.coffeeshop.adapter.LoaispAdapter;
+import com.example.flame.coffeeshop.model.Loaisp;
+import com.example.flame.coffeeshop.util.CheckConnection;
+import com.example.flame.coffeeshop.util.Server;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -27,14 +42,58 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
     ListView listViewmanhinhchinh;
     DrawerLayout drawerLayout;
+    ArrayList<Loaisp> mangloaisp;
+    LoaispAdapter loaispAdapter;
+    int id=0;
+    String tenloaisp="";
+    String hinhanhloaisp="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Anhxa();
-        //ActionBar();
-        ActionViewFlipper();
+        if(CheckConnection.haveNetworkConnection(getApplicationContext())){
+            ActionBar();
+            ActionViewFlipper();
+            GetDuLieuLoaisp();
+        }else{
+            CheckConnection.ShowToast_Short(getApplicationContext(),"Bạn hãy kiểm tra lại kết nối");
+            finish();
+        }
+
+    }
+
+    private void GetDuLieuLoaisp() {
+        final RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Server.DuongdanLoaisp, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if(response != null){
+                    for (int i=0; i<response.length();i++){
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            id=jsonObject.getInt("id");
+                            tenloaisp=jsonObject.getString("tenloaisp");
+                            hinhanhloaisp=jsonObject.getString("hinhanhloaisp");
+                            mangloaisp.add(new Loaisp(id,tenloaisp,hinhanhloaisp));
+                            loaispAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    mangloaisp.add(3,new Loaisp(0,"Liên hệ","http://farmercoffee.com.vn/wp-content/uploads/2015/12/phone.png"));
+                    mangloaisp.add(4,new Loaisp(0,"Thông tin","http://icons.iconarchive.com/icons/icons8/windows-8/512/Very-Basic-About-icon.png"));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                CheckConnection.ShowToast_Short(getApplicationContext(),error.toString());
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
     }
 
     private void ActionViewFlipper() {
@@ -67,6 +126,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     private  void Anhxa(){
         toolbar = (Toolbar) findViewById(R.id.toolbarmanhinhchinh);
         viewFlipper = (ViewFlipper) findViewById(R.id.viewlipper);
@@ -74,5 +134,9 @@ public class MainActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.navigationview);
         listViewmanhinhchinh = (ListView) findViewById(R.id.listviewmanhinhchinh);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mangloaisp = new ArrayList<>();
+        mangloaisp.add(0,new Loaisp(0,"Trang Chính","https://sieuthihue.vn/wp-content/uploads/2018/10/homes.png"));
+        loaispAdapter = new LoaispAdapter(mangloaisp,getApplicationContext());
+        listViewmanhinhchinh.setAdapter(loaispAdapter);
     }
 }
---
+++ app/src/main/java/com/example/flame/coffeeshop/util/Server.java
@@ -0,0 +1,6 @@
+package com.example.flame.coffeeshop.util;
+
+public class Server {
+    public  static String localhost = "192.168.100.5:8080";
+    public  static String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
+}
